b5ecfe4ea0b66dddd5989930002be195f22dfe44
art-Artur <donaldfak1047@gmail.com>
2024-12-02 22:01:37 +0300

    0

--- script.js
+++ script.js
@@ -29,3 +29,38 @@ dots.forEach((dot, index) => {
 });
 
 showSlide(currentSlide);
+
+function toggleTheme() {
+    const body = document.body;
+
+    // Если текущая тема - светлая, переключаем на темную
+    if (body.classList.contains('light-theme')) {
+        body.classList.remove('light-theme');
+        body.classList.add('dark-theme');
+    } else {
+        body.classList.remove('dark-theme');
+        body.classList.add('light-theme');
+    }
+
+    // Сохранить выбор пользователя в локальном хранилище
+    localStorage.setItem('theme', body.classList.contains('dark-theme') ? 'dark' : 'light');
+}
+
+// Загрузить тему из локального хранилища при загрузке страницы
+document.addEventListener('DOMContentLoaded', () => {
+    const savedTheme = localStorage.getItem('theme');
+    const body = document.body;
+
+    // Проверяем сохраненную тему и применяем ее
+    if (savedTheme === 'dark') {
+        body.classList.add('dark-theme');
+    } else {
+        body.classList.add('light-theme');
+    }
+
+    // Добавить обработчик на кнопку смены темы
+    const themeToggleButton = document.getElementById('theme-toggle-button');
+    if (themeToggleButton) {
+        themeToggleButton.addEventListener('click', toggleTheme);
+    }
+});
