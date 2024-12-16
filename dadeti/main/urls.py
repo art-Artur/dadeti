a5135e1c5c0fcfb573e3746bf39235a0e51ac3c8
Forlan <sarper180@gmail.com>
2024-12-16 03:02:07 +0300

    book and author details

--- dadeti/main/urls.py
+++ dadeti/main/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("book", views.book, name="book"),
+    path("book/", views.BookListView.as_view(), name="book"),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
     path("article", views.article, name="article"),
     path("journal", views.journal, name="journal"),
 ]
